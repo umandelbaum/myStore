acd371f6549440810b4636db2fd0b496a8654f5e
Uri Mandelbaum <umandelbaum@gmail.com>
2022-03-03 22:59:33 -0700

    added nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { FormsModule } from '@angular/forms';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductListItemComponent } from './product-list-item/product-list-item.component';
 import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductListItemComponent,
-    ProductItemDetailComponent
+    ProductItemDetailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
